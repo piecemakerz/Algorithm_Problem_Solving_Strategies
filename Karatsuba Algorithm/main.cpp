4913aafc67523464f425a8b600885374e0754209
piecemakerz <piecemakerz@naver.com>
2017-08-14 13:16:57 +0900

    카라츠바 곱셈 알고리즘 구현 - pg 183

--- Karatsuba Algorithm/main.cpp
+++ Karatsuba Algorithm/main.cpp
@@ -26,7 +26,7 @@ vector<int> multiply(const vector<int>& a, const vector<int>& b) {
 	vector<int> c(a.size() + b.size() + 1, 0);
 	for (int i = 0; i<a.size(); i++)
 		for (int j = 0; j<b.size(); j++)
-			c[i + j] = a[i] * b[j];
+			c[i + j] += a[i] * b[j];
 	normalize(c);
 	return c;
 }
