1dbf428e7cc8911d6b19f08d517a7859930f7687
piecemakerz <piecemakerz@naver.com>
2017-10-16 13:38:11 +0900

    보글 게임 구현 2차 - pg 150

--- BoggleGame/BoggleGame/main.cpp
+++ BoggleGame/BoggleGame/main.cpp
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <string>
 
 #define BOGGLE_LEN 5
 using namespace std;
@@ -60,8 +62,71 @@ bool hasWordMine(int y, int x, char * word, int picked = 1) {
 	return false;
 }
 
+// 직접 구현한 함수2
+/*char box[BOGGLE_LEN][BOGGLE_LEN] = { { 'N','N','N','N','S' },
+{ 'N','E','E','E','N' },
+{ 'N','E','Y','E','N' },
+{ 'N','E','E','E','N' },
+{ 'N','N','N','N','N' } };
+
+bool checked[BOGGLE_LEN][BOGGLE_LEN] = { false, };
+
+bool hasWord(int, int, string);
+void ShowBoard();
+
+int main()
+{
+	int x = 2, y = 2;
+	string word;
+	ShowBoard();
+	printf("\nType Word to Find : ");
+	cin >> word;
+	cout<<hasWord(y, x, word)<<endl;
+	return 0;
+}
+
+bool hasWord(int y, int x, string word) {
+	bool hasword = false;
+
+	if ((y >= BOGGLE_LEN || y < 0) || (x >= BOGGLE_LEN || x < 0)) {
+		return false;
+	}
+
+	checked[y][x] = true;
+
+	if (word[0] == box[y][x]) {
+
+		if (word.size() == 1)
+			return true;
+
+		else {
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					if ((i == 0 && j == 0) || checked[y+i][x+j] == true)
+						continue;
+					if (hasWord(y + i, x + j, word.substr(1)))
+						hasword = true;
+				}
+			}
+		}
+	}
+
+	return hasword;
+}
+
+void ShowBoard() {
+	for (int i = 0; i < BOGGLE_LEN; i++) {
+		for (int j = 0; j < BOGGLE_LEN; j++) {
+			printf("%c ", box[i][j]);
+		}
+		printf("\n");
+	}
+}
+*/
+
 int main(void) {
 	printf("%d\n", hasWord(2, 2, "YES"));
 	printf("%d\n", hasWordMine(2, 2, "YES"));
 	return 0;
 }
+
