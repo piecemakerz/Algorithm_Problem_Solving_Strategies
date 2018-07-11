1c62008055435a63dbb826b21ed9f180aa6c4924
piecemakerz <piecemakerz@naver.com>
2018-07-11 23:32:23 +0900

    algorithm resolved

--- Chapter 9. 동적 계획법 테크닉/k번째 답 계산하기/DRAGON/DRAGON/main.cpp
+++ Chapter 9. 동적 계획법 테크닉/k번째 답 계산하기/DRAGON/DRAGON/main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int c, n, p, l;
 
-int curve(string curStr, int curP, int age) {
-	if (age == n && curP >= p) {
+const int MAX = 1000000051;
+
+int maxcache[51];
+
+int findMaxLength(int ageLeft) {
+	int& ret = maxcache[ageLeft];
+	if (ret != -1) return ret;
+
+	int length = 1;
+	for (int i = 1; i <= ageLeft; i++) {
+		length = min(MAX, length + (3 * ageLeft));
+	}
+	return length;
+}
+
+int curve2(string curStr, int curP, int age) {
+	if (l <= 0 || curP >= MAX)
+		return MAX;
+
+	if (age == n && l > 0 && curP >= p) {
 		int idx = 0;
-		while (l != 0 && idx < curStr.length()) {
+		while (l > 0 && idx < curStr.length()) {
 			cout << curStr[idx++];
 			l--;
 			curP++;
@@ -15,14 +34,58 @@ int curve(string curStr, int curP, int age) {
 	}
 
 	for (int i = 0; i < curStr.length(); i++) {
+		if (curStr[i] == 'X' || curStr[i] == 'Y') {
+			if (curP + findMaxLength(n - age) <= p) {
+				curP += findMaxLength(n - age) + 1;
+				continue;
+			}
+
+			if (curStr[i] == 'X')
+				curP = curve2("X+YF", curP, age + 1);
+			else
+				curP = curve2("FX-Y", curP, age + 1);
+		}
+		else
+			if (curP >= p && l > 0) {
+				cout << curStr[i];
+				l--;
+				curP++;
+			}
+	}
+	return curP;
+}
+
+//모든 답을 확인해보는 완전탐색 알고리즘. 답은 맞게 나온다.
+int curve(string curStr, int curP, int age) {
+	if (l <= 0 || curP >= MAX)
+		return MAX;
+
+	int len = curStr.length();
+	if (age == n){
+		if(curP + len <= p)
+			return curP + len;
+		else{
+			int idx = 0;
+			while (l > 0 && idx < curStr.length()) {
+				if (curP + idx >= p) {
+					cout << curStr[idx++];
+					l--;
+				}
+				curP++;
+			}
+			return curP;
+		}
+	}
+
+	for (int i = 0; i < curStr.length(); i++) {
 		if (curStr[i] == 'X') {
-			curP = curve("+YF", curP + 1, age + 1);
+			curP = curve("X+YF", curP, age + 1);
 		}
 		else if (curStr[i] == 'Y') {
 			curP = curve("FX-Y", curP, age + 1);
 		}
 		else {
-			if (curP >= p && l >= 0) {
+			if (curP >= p && l > 0) {
 				cout << curStr[i];
 				l--;
 			}
@@ -35,8 +98,9 @@ int curve(string curStr, int curP, int age) {
 int main(void) {
 	cin >> c;
 	for (int test = 0; test < c; test++) {
+		memset(maxcache, -1, sizeof(maxcache));
 		cin >> n >> p >> l;
-		curve("FX", 1, 0);
+		curve2("FX", 1, 0);
 		cout << endl;
 	}
 	return 0;
