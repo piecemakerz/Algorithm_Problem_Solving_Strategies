c21800906c0b48c2f43117a6adf21d99d62b155d
piecemakerz <piecemakerz@naver.com>
2018-07-17 10:20:09 +0900

    algorithm resolved

--- Chapter 9. 동적 계획법 테크닉/조합 게임/BLOCKGAME/BLOCKGAME/main.cpp
+++ Chapter 9. 동적 계획법 테크닉/조합 게임/BLOCKGAME/BLOCKGAME/main.cpp
@@ -14,13 +14,60 @@ int block[6][2][2] = {
 	{{1,0}}
 };
 
-bool gameplay(int state, bool turn) {
+int cache[1 << 25][2];
+int gameplay(int state, bool turn, int curX, int curY) {
+	if (curY >= 5)
+		return turn;
+	else if (curX >= 5)
+		return gameplay(state, turn, 0, curY + 1);
 
+	int& ret = cache[state][turn];
+	if (ret != -1) return ret;
+	ret = false;
+
+	int curBlock = board[curY][curX];
+	if (curBlock != 0) 
+		return ret = gameplay(state | curBlock, !turn, curX + 1, curY);
+
+	bool blockPlaced = false;
+	for (int i = 0; i < 4; i++) {
+		int subX1 = block[i][0][1], subX2 = block[i][1][1];
+		int subY1 = block[i][0][0], subY2 = block[i][1][0];
+		if (board[subY1][subX1] != 0 && board[subY2][subX2] != 0) {
+			blockPlaced = true;
+			board[subY1][subX1] = 1, board[subY2][subX2] = 1;
+			ret = gameplay(state | curBlock, !turn, curX + 1, curY);
+			board[subY1][subX1] = 0, board[subY2][subX2] = 0;
+			if (ret == true)
+				return ret;
+		}
+	}
+
+	for (int i = 4; i < 6; i++) {
+		int subX = block[i][0][1], subY = block[i][0][0];
+		if (board[subY][subX] != 0) {
+			blockPlaced = true;
+			board[subY][subX] = 1;
+			ret = gameplay(state | curBlock, !turn, curX + 1, curY);
+			board[subY][subX] = 0;
+			if (ret == true)
+				return ret;
+		}
+	}
+
+	if (!blockPlaced) {
+		board[curY][curX] = 1;
+		return ret = gameplay(state | curBlock, !turn, curX + 1, curY);
+		board[curY][curX] = 0;
+	}
+
+	return ret;
 }
 
 int main(void) {
 	cin >> C;
 	for (int test = 0; test < C; test++) {
+		memset(cache, -1, sizeof(cache));
 		int state = 0;
 		char input;
 		for (int y = 0; y < 5; y++)
@@ -34,7 +81,7 @@ int main(void) {
 				}
 			}
 
-		if (gameplay(state, true))
+		if (gameplay(state, false, 0, 0))
 			cout << "WINNING" << endl;
 		else
 			cout << "LOSING" << endl;
