be8e51e1d69c36a187b321e0c16bc22b39fc1216
piecemakerz <piecemakerz@naver.com>
2018-08-01 22:40:52 +0900

    algorithm resolved

--- Chapter 15. 계산 기하/2차원 벡터의 표현/2차원 벡터의 표현/main.cpp
+++ Chapter 15. 계산 기하/2차원 벡터의 표현/2차원 벡터의 표현/main.cpp
@@ -105,3 +105,25 @@ bool segmentIntersection(vector2 a, vector2 b,
 	return isBoundingRectangle(p, a, b) &&
 		isBoundingRectangle(p, c, d);
 }
+//두 선분의 교차 여부를 좀 더 간단하게 확인하는 segmentIntersects() 함수의 구현
+//두 선분이 서로 접촉하는지 여부를 반환한다.
+bool segmentIntersects(vector2 a, vector2 b, vector2 c, vector2 d) {
+	double ab = ccw(a, b, c) * ccw(a, b, d);
+	double cd = ccw(c, d, a) * ccw(c, d, b);
+	//두 선분이 한 직선 위에 있거나 끝점이 겹치는 경우
+	if (ab == 0 && cd == 0) {
+		if (b < a) swap(a, b);
+		if (d < c) swap(c, d);
+		return !(b < c || d < a);
+	}
+	return ab <= 0 && cd <= 0;
+}
+//점과 선 사이의 거리를 계산하는 함수 pointToLine()의 구현
+//점 p에서 (a, b) 직선에 내린 수선의 발을 구한다.
+vector2 perpendicularFoot(vector2 p, vector2 a, vector2 b) {
+	return a + (p - a).project(b - a);
+}
+//점 p와 (a, b) 직선 사이의 거리를 구한다.
+double pointToLine(vector2 p, vector2 a, vector2 b) {
+	return  (p - perpendicularFoot(p, a, b)).norm();
+}
